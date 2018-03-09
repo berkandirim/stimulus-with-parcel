bd319a14c1c61f8667c93fa2ba02dd1f45cba62f
Berkan Dirim <berkan.dirim@dogantv.com.tr>
2018-03-09 09:00:23 +0300

    add some css

--- src/controllers/refreshable_controller.js
+++ src/controllers/refreshable_controller.js
@@ -1,9 +1,13 @@
 import { Controller } from "stimulus"
 
+const data = {
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur laudantium, quam quos neque praesentium magni laborum natus harum molestiae beatae! Natus unde harum similique corporis, aut magni. Illo, dolore consequatur.'
+}
+
 export default class extends Controller {
     static targets = [ "content", "headline" ]
     refresh () {
         this.headlineTarget.innerHTML = "Hello"
-        this.contentTarget.innerHTML = "Refresh!"
+        this.contentTarget.innerHTML = data.text
     }
 }
